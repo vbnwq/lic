6deb414ddcf7ad8e086fb5a08aec06a1ffdf5cd9
vbnwq <mrreza129@gmail.com>
2024-08-01 19:28:27 +0330

    Update license file

--- lice.js
+++ lice.js
@@ -3,6 +3,10 @@
     {
       "license": "prfprf",
       "expiresAt": "2024-08-11T15:58:14.881214Z"
+    },
+    {
+      "license": "test",
+      "expiresAt": "2024-08-01T15:58:27.152804Z"
     }
   ]
 }
